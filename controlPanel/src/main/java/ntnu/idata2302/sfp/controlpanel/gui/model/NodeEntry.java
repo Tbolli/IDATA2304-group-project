15e2ddc674d4d396d4d4549f6e57fc923adcb278
Nathaniel <ilardenathaniel@gmail.com>
2025-11-20 23:50:54 +0100

    Added javadoc comments and fixed google checkstyle on NodeEntry class

--- controlPanel/src/main/java/ntnu/idata2302/sfp/controlpanel/gui/model/NodeEntry.java
+++ controlPanel/src/main/java/ntnu/idata2302/sfp/controlpanel/gui/model/NodeEntry.java
@@ -14,22 +14,53 @@ public class NodeEntry {
   private final int nodeId;
   private final ObjectProperty<DataReportBody> data = new SimpleObjectProperty<>();
 
+  /**
+   * Creates a new NodeEntry with a given node ID and initial data.
+   *
+   * @param nodeId the identifier of the sensor node
+   * @param data   the initial data report associated with this node
+   */
+
   public NodeEntry(int nodeId, DataReportBody data) {
     this.nodeId = nodeId;
     this.data.set(data);
   }
 
+  /**
+   * Returns the node ID associated with this entry.
+   *
+   * @return the sensor node identifier
+   */
+
   public int nodeId() {
     return nodeId;
   }
+
+  /**
+   * Returns the current {@link DataReportBody} stored in this entry.
+   *
+   * @return the latest data report, or {@code null} if none is set
+   */
 
   public DataReportBody getData() {
     return data.get();
   }
 
+  /**
+   * Updates the data report for this node.
+   *
+   * @param d the new {@link DataReportBody} to set
+   */
+
   public void setData(DataReportBody d) {
     this.data.set(d);
   }
+
+  /**
+   * Returns the observable JavaFX property for the node's data.
+   *
+   * @return the data property
+   */
 
   public ObjectProperty<DataReportBody> dataProperty() {
     return data;
