e9cca2e9f2414a97a460092c4725872f16e2f944
Nathaniel <ilardenathaniel@gmail.com>
2025-11-20 23:21:54 +0100

    Added javadoc comments and fixed google checkstyle on Body interface

--- library/src/main/java/ntnu/idata2302/sfp/library/body/Body.java
+++ library/src/main/java/ntnu/idata2302/sfp/library/body/Body.java
@@ -1,5 +1,16 @@
 package ntnu.idata2302.sfp.library.body;
 
+/**
+ * Marker interface for message bodies in the Smart Farming Protocol.
+ */
+
 public interface Body {
+
+  /**
+   * Serialize this body to CBOR bytes.
+   *
+   * @return a byte array containing the CBOR-encoded representation of this instance
+   */
+
   byte[] toCbor();
 }
